96b07ee237bda1309a9bd083aec2627500b8402c
RenHeyzer <vesolyyradin@gmail.com>
2025-02-25 20:29:25 +0600

    feat: new OutlinedTextField with error text at the bottom

---
+++ app/src/main/java/dev/renheyzer/tazalyk/presentation/utils/OutlinedErrorTextField.kt
@@ -0,0 +1,89 @@
+package dev.renheyzer.tazalyk.presentation.utils
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import dev.renheyzer.tazalyk.ui.theme.TazalykTheme
+
+@Composable
+fun OutlinedErrorTextField(
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    errorText: String,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    singleLine: Boolean = false,
+    maxLines: Int = if (singleLine) 1 else Int.MAX_VALUE,
+    minLines: Int = 1,
+    interactionSource: MutableInteractionSource? = null,
+    shape: Shape = OutlinedTextFieldDefaults.shape,
+) {
+    Column(modifier) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            readOnly = readOnly,
+            textStyle = textStyle,
+            isError = isError,
+            label = label,
+            placeholder = placeholder,
+            leadingIcon = leadingIcon,
+            trailingIcon = trailingIcon,
+            prefix = prefix,
+            suffix = suffix,
+            supportingText = supportingText,
+            visualTransformation = visualTransformation,
+            keyboardOptions = keyboardOptions,
+            keyboardActions = keyboardActions,
+            singleLine = singleLine,
+            maxLines = maxLines,
+            minLines = minLines,
+            interactionSource = interactionSource,
+            shape = shape,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedBorderColor = TazalykTheme.colors.borderColor,
+                focusedBorderColor = TazalykTheme.colors.borderColor,
+                unfocusedLabelColor = TazalykTheme.colors.secondaryText,
+                focusedLabelColor = TazalykTheme.colors.primaryText,
+                errorBorderColor = TazalykTheme.colors.errorColor,
+                errorLabelColor = TazalykTheme.colors.errorColor,
+            )
+        )
+        if (isError) {
+            Text(
+                modifier = Modifier.padding(start = 10.dp),
+                text = errorText,
+                color = TazalykTheme.colors.errorColor
+            )
+        }
+    }
+}
