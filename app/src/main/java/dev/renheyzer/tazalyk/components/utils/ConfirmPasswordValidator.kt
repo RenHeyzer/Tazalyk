5ca75abcfeb3a5f2660fd82faaa3e42f353586e0
RenHeyzer <vesolyyradin@gmail.com>
2025-03-02 23:40:47 +0600

    refactor(utils): change return type to `BaseValidator`

--- app/src/main/java/dev/renheyzer/tazalyk/components/utils/ConfirmPasswordValidator.kt
+++ app/src/main/java/dev/renheyzer/tazalyk/components/utils/ConfirmPasswordValidator.kt
@@ -1,6 +1,6 @@
 package dev.renheyzer.tazalyk.components.utils
 
-class ConfirmPasswordValidator(private val password: String) : Validator {
+class ConfirmPasswordValidator(private val password: String) : BaseValidator() {
     override fun validate(data: String): Result<Boolean> {
         return if (data == password) {
             Result.success(true)
