0119e7455bbb045e959d1565b9f931a6b5166753
RenHeyzer <vesolyyradin@gmail.com>
2025-03-02 19:00:32 +0600

    refactor: change return type to Result<Boolean>

--- app/src/main/java/dev/renheyzer/tazalyk/components/signup/utils/EmailValidator.kt
+++ app/src/main/java/dev/renheyzer/tazalyk/components/signup/utils/EmailValidator.kt
@@ -2,12 +2,13 @@ package dev.renheyzer.tazalyk.components.signup.utils
 
 import android.util.Patterns
 import dev.renheyzer.tazalyk.components.utils.BaseValidator
+import dev.renheyzer.tazalyk.components.utils.Validator
 
 class EmailValidator : BaseValidator() {
 
-    override fun validate(data: String): Boolean {
-        if (!Patterns.EMAIL_ADDRESS.matcher(data).matches())
-            throw IllegalArgumentException("Недопустимый адрес электронной почты")
-        return true
+    override fun validate(data: String): Result<Boolean> {
+        return if (!Patterns.EMAIL_ADDRESS.matcher(data).matches())
+           Result.failure(IllegalArgumentException("Недопустимый адрес электронной почты"))
+        else Result.success(true)
     }
 }
