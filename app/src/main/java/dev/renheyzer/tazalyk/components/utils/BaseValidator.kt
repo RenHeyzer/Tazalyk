ef042d8533877d0162ed75f1a5bdec466e9655f0
RenHeyzer <vesolyyradin@gmail.com>
2025-03-02 19:00:25 +0600

    refactor: change return type to Result<Boolean>

--- app/src/main/java/dev/renheyzer/tazalyk/components/utils/BaseValidator.kt
+++ app/src/main/java/dev/renheyzer/tazalyk/components/utils/BaseValidator.kt
@@ -2,10 +2,10 @@ package dev.renheyzer.tazalyk.components.utils
 
 abstract class BaseValidator(private val next: Validator? = null) : Validator {
 
-    override fun validate(data: String): Boolean {
+    override fun validate(data: String): Result<Boolean> {
         if (this.next != null) {
             next.validate(data)
         }
-        return false
+        return Result.failure(IllegalStateException("There is no next validator"))
     }
 }
--- app/src/main/java/dev/renheyzer/tazalyk/components/utils/BlankValidator.kt
+++ app/src/main/java/dev/renheyzer/tazalyk/components/utils/BlankValidator.kt
@@ -2,9 +2,9 @@ package dev.renheyzer.tazalyk.components.utils
 
 class BlankValidator(private val next: Validator? = null) : BaseValidator(next) {
 
-    override fun validate(data: String): Boolean {
+    override fun validate(data: String): Result<Boolean> {
         return if (data.isNotBlank()) {
-            true
+            Result.success(true)
         } else {
             super.validate(data)
         }
