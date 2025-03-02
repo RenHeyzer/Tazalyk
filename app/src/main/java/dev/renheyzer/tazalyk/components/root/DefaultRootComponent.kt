a03f790e2b64a7bf0e74b6b908e3efb4484fd955
RenHeyzer <vesolyyradin@gmail.com>
2025-03-02 19:05:41 +0600

    feat: confirm and create password components
    - Renamed SignInComponent to LoginComponent

--- app/src/main/java/dev/renheyzer/tazalyk/components/root/DefaultRootComponent.kt
+++ app/src/main/java/dev/renheyzer/tazalyk/components/root/DefaultRootComponent.kt
@@ -4,8 +4,13 @@ import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.router.stack.childStack
 import com.arkivanov.decompose.router.stack.pushNew
-import dev.renheyzer.tazalyk.components.signin.RealSignInComponent
-import dev.renheyzer.tazalyk.components.signin.SignInComponent
+import dev.renheyzer.tazalyk.components.confirm.ConfirmArgs
+import dev.renheyzer.tazalyk.components.confirm.ConfirmComponent
+import dev.renheyzer.tazalyk.components.confirm.RealConfirmComponent
+import dev.renheyzer.tazalyk.components.createpassword.CreatePasswordComponent
+import dev.renheyzer.tazalyk.components.createpassword.RealCreatePasswordComponent
+import dev.renheyzer.tazalyk.components.signin.LoginComponent
+import dev.renheyzer.tazalyk.components.signin.RealLoginComponent
 import dev.renheyzer.tazalyk.components.signup.RealSignUpComponent
 import dev.renheyzer.tazalyk.components.signup.SignUpComponent
 import dev.renheyzer.tazalyk.components.utils.toStateFlow
@@ -29,21 +34,58 @@ class DefaultRootComponent(
         config: Config,
         componentContext: ComponentContext
     ): RootComponent.Child = when (config) {
-            Config.SignIn -> RootComponent.Child.SignIn(signIn(componentContext))
-            Config.SignUp -> RootComponent.Child.SignUp(signUp(componentContext))
-        }
+        Config.Login -> RootComponent.Child.SignIn(login(componentContext))
+        Config.SignUp -> RootComponent.Child.SignUp(signUp(componentContext))
+        is Config.Confirm -> RootComponent.Child.Confirm(confirm(componentContext, config))
+        Config.CreatePassword -> RootComponent.Child.CreatePassword(createPassword(componentContext))
+    }
 
-    private fun signIn(componentContext: ComponentContext): SignInComponent =
-        RealSignInComponent(
+    private fun login(componentContext: ComponentContext): LoginComponent =
+        RealLoginComponent(
             componentContext = componentContext,
+            navigateToMain = {},
+            navigateToSignUp = {
+                navigation.pushNew(Config.SignUp)
+            }
         )
 
     private fun signUp(componentContext: ComponentContext): SignUpComponent =
         RealSignUpComponent(
             componentContext = componentContext,
-            navigateToConfirm = {},
+            navigateToConfirm = { inputValue, isPhoneInput ->
+                navigation.pushNew(
+                    Config.Confirm(
+                        ConfirmArgs(
+                            inputValue = inputValue,
+                            isPhoneInput = isPhoneInput
+                        )
+                    )
+                )
+            },
             navigateToSignIn = {
-                navigation.pushNew(Config.SignIn)
+                navigation.pushNew(Config.Login)
+            }
+        )
+
+    private fun confirm(
+        componentContext: ComponentContext,
+        config: Config.Confirm
+    ): ConfirmComponent =
+        RealConfirmComponent(
+            componentContext = componentContext,
+            args = config.args,
+            navigateToCreatePassword = {
+                navigation.pushNew(Config.CreatePassword)
+            }
+        )
+    
+    private fun createPassword(
+        componentContext: ComponentContext
+    ): CreatePasswordComponent =
+        RealCreatePasswordComponent(
+            componentContext = componentContext,
+            navigateToLogin = {
+                navigation.pushNew(Config.Login)
             }
         )
 
@@ -54,6 +96,12 @@ class DefaultRootComponent(
         data object SignUp : Config()
 
         @Serializable
-        data object SignIn : Config()
+        data object Login : Config()
+
+        @Serializable
+        data class Confirm(val args: ConfirmArgs) : Config()
+
+        @Serializable
+        data object CreatePassword : Config()
     }
 }
--- app/src/main/java/dev/renheyzer/tazalyk/components/root/RootComponent.kt
+++ app/src/main/java/dev/renheyzer/tazalyk/components/root/RootComponent.kt
@@ -1,7 +1,9 @@
 package dev.renheyzer.tazalyk.components.root
 
 import com.arkivanov.decompose.router.stack.ChildStack
-import dev.renheyzer.tazalyk.components.signin.SignInComponent
+import dev.renheyzer.tazalyk.components.confirm.ConfirmComponent
+import dev.renheyzer.tazalyk.components.createpassword.CreatePasswordComponent
+import dev.renheyzer.tazalyk.components.signin.LoginComponent
 import dev.renheyzer.tazalyk.components.signup.SignUpComponent
 import kotlinx.coroutines.flow.StateFlow
 
@@ -11,6 +13,8 @@ interface RootComponent {
 
     sealed class Child {
         class SignUp(val component: SignUpComponent) : Child()
-        class SignIn(val component: SignInComponent) : Child()
+        class SignIn(val component: LoginComponent) : Child()
+        class Confirm(val component: ConfirmComponent) : Child()
+        class CreatePassword(val component: CreatePasswordComponent) : Child()
     }
 }
