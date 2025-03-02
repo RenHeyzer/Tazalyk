ec9ece15e5f914af3dec9bf08542d81b85646390
RenHeyzer <vesolyyradin@gmail.com>
2025-03-02 19:42:04 +0600

    feat: setup Koin and `appModule`

--- app/src/main/java/dev/renheyzer/tazalyk/TazalykApp.kt
+++ app/src/main/java/dev/renheyzer/tazalyk/TazalykApp.kt
@@ -1,5 +1,20 @@
 package dev.renheyzer.tazalyk
 
 import android.app.Application
+import dev.renheyzer.tazalyk.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
-class TazalykApp : Application()
+class TazalykApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@TazalykApp)
+            modules(appModule)
+        }
+    }
+}
